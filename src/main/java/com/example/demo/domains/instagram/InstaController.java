0ab5a67545de6894190b75b3da91e7dc8d1be506
jinwoo <chaindevchu@gmail.com>
2025-04-04 10:12:09 +0900

    tymeleaf 설정추가, test page 추가

---
+++ src/main/java/com/example/demo/domains/instagram/InstaController.java
@@ -0,0 +1,15 @@
+package com.example.demo.domains.instagram;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "insta")
+public class InstaController {
+
+    @GetMapping("/test")
+    public String testPage1() {
+        return "test";
+    }
+}
