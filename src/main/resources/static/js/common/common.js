607a7c8d20f0aae21a7dfa82d8dbbbc460b9fe5a
jinwoo <chaindevchu@gmail.com>
2025-04-04 16:53:47 +0900

    ajax 모듈 수정

--- src/main/resources/static/js/common/common.js
+++ src/main/resources/static/js/common/common.js
@@ -1,147 +1,77 @@
 this.Utils = {
-    ajax : (params) => {
-        let url   = params.url;
-        if(url) {
-            url = Utils.getUrl(url);
-            const mask     = Utils.nvl(params.mask,true);
-            const type     = Utils.nvl(params.type    ,"POST");
-            const dataType = Utils.nvl(params.dataType,"json");
-            const contentType = Utils.nvl(params.contentType,"application/json");
-            const async    = Utils.nvl(params.async   ,true);
-            const global = (!async && mask) ? true : false;
-            const defaultData = {
-                progPh: Utils.getProgPath()
-            };
+    ajax: (params) => {
+        let url = Utils.getUrl(params.url);
+        if (!url) return;
 
-            const titleObj = $(".main-title,.modal-title");
-            if(titleObj.length > 0) {
-                defaultData.actNm = titleObj.text();
+        const mask = Utils.nvl(params.mask, true);
+        const type = Utils.nvl(params.type, "POST");
+        const dataType = Utils.nvl(params.dataType, "json");
+        const contentType = Utils.nvl(params.contentType, "application/json");
+        const async = Utils.nvl(params.async, true);
+
+        const defaultData = {
+            progPh: Utils.getProgPath(),
+        };
+
+        let maskObj = mask ? new Utils.loadMask() : null;
+        if (maskObj) maskObj.open();
+
+        let data = params.data;
+        let isFormData = data instanceof FormData;
+
+        if (isFormData) {
+            for (let key in defaultData) {
+                data.append(key, defaultData[key]);
             }
-
-            let maskObj = (mask) ? new Utils.loadMask() : null;
-            (mask) ? maskObj.open() : false;
-
-            let data = params.data;
-            let isFormData = false;
-            if(data){
-                if(data instanceof FormData){
-                    for(let key in defaultData){
-                        data.append(key,defaultData[key]);
-                    }
-                    isFormData = true;
-                }else{
-                    if(Utils.isObject(params.data)){
-                        data = $.extend({},defaultData, params.data||{});
-                    }else{
-                        data = $.extend({},defaultData, JSON.parse(params.data||{}));
-                        data = JSON.stringify(data);
-                    }
-                }
+        } else if (Utils.isObject(data)) {
+            data = $.extend({}, defaultData, data);
+            if (contentType.includes("json")) {
+                data = JSON.stringify(data);
             }
-            /*if(!Utils.isEmptyObject(data)){
-                if(sendType === "json") {
-                    //data = JSON.stringify(data);
-                }
-            }*/
-
-            let config  = {
-                url,
-                type,
-                dataType,
-                contentType,
-                async,
-                //global,
-                data,
-                error : (xhr, status, error) => {
-                    if(mask) maskObj.close();
-                    //에러 핸들링 은 수정할꺼임..
-                    let resData  = xhr.responseJSON||xhr.responseText||{};
-                    if(!Utils.isEmptyObject(resData)){
-                        if(typeof resData == "string"){
-                            resData = JSON.parse(resData);
-                        }
-                        const message = Utils.nvl(resData.message,"오류가 발생하였습니다.\n관리자에게 문의해주세요.");
-                        if(message === "SESSION_EXPIRED"){
-                            Utils.sessionExpired(true);
-                        }else{
-                            alert(message);
-                        }
-                    }else{
-                        alert("오류가 발생하였습니다.\n관리자에게 문의해주세요.");
-                    }
-                },
-                /*beforeSend: function(xhr) {
-                    //if(mask && maskObj)	maskObj.open();
-                },*/
-                complete: function () {
-                    if(mask && maskObj)	maskObj.close();
-                }
-            }
-
-            if(params.success && Utils.isFunction(params.success)) {
-                config.success = function(res){
-                    if(mask && maskObj) maskObj.close();
-                    params.success(res);
-                }
-            }
-            if(params.error && Utils.isFunction(params.error)) {
-                config.error = (xhr, status, error) => {
-                    if(mask && maskObj) maskObj.close();
-                    let resData  = xhr.responseJSON||xhr.responseText||{};
-                    if(typeof resData == "string"){
-                        resData = JSON.parse(resData);
-                    }
-                    params.error(resData);
-                }
-            }
-
-            if(isFormData) {
-                config.contentType = false;
-                config.processData = false;
-            }
-
-            /*if(!async) {
-                 const res = await new Promise((resolve, reject) => {
-                    $.ajax({
-                        ...config,
-                        success: resolve,
-                        error: (xhr) => {
-                            let resData = xhr.responseJSON || xhr.responseText || {};
-                            if (typeof resData === "string") {
-                                resData = JSON.parse(resData);
-                            }
-                            reject(resData);
-                        }
-                    });
-                });
-                if(params.success && Utils.isFunction(params.success)) {
-                    if(mask && maskObj) maskObj.close();
-                    params.success(res);
-                }
-                return res;
-            }else{
-                return $.ajax(config);
-            }*/
-
-            let res = {};
-            if(!async) {
-                if(mask){
-                    setTimeout(function(){
-                        res = $.ajax(config);
-                    },100);
-
-                }else{
-                    res = $.ajax(config);
-                    let resData = {};
-                    if(!async) {
-                        resData = res.responseJSON;
-                    }
-                    return resData;
-                }
-            }else{
-                res = $.ajax(config);
-            }
-
         }
-    },
-}
+
+        const config = {
+            url,
+            type,
+            dataType,
+            contentType,
+            async,
+            data,
+            processData: !isFormData,
+            contentType: isFormData ? false : contentType,
+            complete: () => {
+                if (maskObj) maskObj.close();
+            },
+            success: (res) => {
+                if (params.success && Utils.isFunction(params.success)) {
+                    params.success(res);
+                }
+            },
+            error: (xhr) => {
+                if (maskObj) maskObj.close();
+
+                let resData = xhr.responseJSON || xhr.responseText || {};
+                if (typeof resData === "string") {
+                    try {
+                        resData = JSON.parse(resData);
+                    } catch (e) {
+                        resData = {};
+                    }
+                }
+
+                if (params.error && Utils.isFunction(params.error)) {
+                    return params.error(resData);
+                }
+
+                const message = Utils.nvl(resData.message, "오류가 발생하였습니다.\n관리자에게 문의해주세요.");
+                if (message === "SESSION_EXPIRED") {
+                    Utils.sessionExpired(true);
+                } else {
+                    alert(message);
+                }
+            }
+        };
+
+        return $.ajax(config);
+    }
+};
