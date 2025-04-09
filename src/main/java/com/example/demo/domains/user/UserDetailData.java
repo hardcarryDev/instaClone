be9c9393f8597db4f80f9e4a771a73a1e9c6e0a6
jinwoo <chaindevchu@gmail.com>
2025-04-09 10:06:53 +0900

    at > date

--- src/main/java/com/example/demo/domains/user/UserDetailData.java
+++ src/main/java/com/example/demo/domains/user/UserDetailData.java
@@ -18,11 +18,18 @@ public class UserDetailData implements UserDetails {
     private String phone;
     private String email;
     private String memo;
-    private String createdAt;
+    private String createdDate;
     private String createdBy;
-    private String updatedAt;
+    private String updatedDate;
     private String updatedBy;
     private String useYn;
+    private int psErrorCnt;
+    private String userStatus;
+
+    // DB 컬럼이 아니라 쿼리로 계산되서 메모리에 존재하는 데이터
+
+    private int psLockMin;
+    private int psUpdateDay;
 
     // 권한이 없다면 빈 리스트를 리턴 (필요 시 수정 가능)
     @Override
